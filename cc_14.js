6ca0ebe7326c4bf25c8d1d33e5e0866a429c5dc8
mallikanicole <malmichael23@gmail.com>
2025-03-09 14:28:43 -0400

    test t2

--- cc_14.js
+++ cc_14.js
@@ -6,3 +6,14 @@ ticketCard.setAttribute("class","ticket-card");
 ticketCard.setAttribute("id","ticketName")
 
 ticketContainer.appendChild(ticketCard);
+
+function addSupportTicket(ticketName,issueDesc,priorityLev){
+    const supportTicket=document.getElementById('supportTicket')
+    const newTicket=document.createElement('li');
+    newTicket.setAttribute('class','support-ticket');
+    newTicket.innerText=ticketName;
+    newTicket.addEventListener('click',()=>{
+        removeSupportTicket(newTicket);
+    });
+    supportTicket.appendChild(newTicket);
+};
