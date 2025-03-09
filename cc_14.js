14079925e6e57badb1b49db59fe318ca8c78612d
mallikanicole <malmichael23@gmail.com>
2025-03-09 14:15:50 -0400

    test task 2

---
+++ cc_14.js
@@ -0,0 +1,8 @@
+//Task 2 Adding Support Tickets Dynamically
+
+const elementID=document.getElementById('ticketContainer');
+const ticketCard=document.createElement('div');
+ticketCard.setAttribute("class","ticket-card");
+ticketCard.setAttribute("id","ticketName")
+
+ticketContainer.appendChild(ticketCard);
